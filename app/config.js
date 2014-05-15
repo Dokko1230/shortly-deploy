be574427f6162a02d613c1d0369df68b1729f84c
Hack Reactor Students <accounts@hackreactor.com>
2014-05-15 16:52:14 -0700

    adds user mongo db

--- app/config.js
+++ app/config.js
@@ -1,21 +1,12 @@
 var Bookshelf = require('bookshelf');
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var Promise = require('bluebird');
+var crypto = require('crypto');
 
 mongoose.connect(process.env.MONGO_ENV);
 
 var path = require('path');
-
-// var db = Bookshelf.initialize({
-//   client: 'sqlite3',
-//   connection: {
-//     host: '127.0.0.1',
-//     user: 'your_database_user',
-//     password: 'password',
-//     database: 'shortlydb',
-//     charset: 'utf8',
-//     filename: path.join(__dirname, '../db/shortly.sqlite')
-//   }
-// });
 
 var linksSchema = new mongoose.Schema({
   url: String,
@@ -26,41 +17,31 @@ var linksSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now }
 });
 
-linksSchema.static('onSaveHandler', function(link, callback) {
-  console.log('whutt');
+linksSchema.pre('save', function(next) {
+  var shasum = crypto.createHash('sha1');
+  shasum.update(this.url);
+  this.code = shasum.digest('hex').slice(0, 5);
+  next();
 });
 
 exports.linksSchema = linksSchema;
 
+var usersSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  createdAt: { type: Date, default: Date.now }
+});
 
+usersSchema.method('comparePassword', function(attemptedPassword, callback){
+  bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
+      callback(isMatch);
+  });
+});
 
-// db.knex.schema.hasTable('urls').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('urls', function (link) {
-//       link.increments('id').primary();
-//       link.string('url', 255);
-//       link.string('base_url', 255);
-//       link.string('code', 100);
-//       link.string('title', 255);
-//       link.integer('visits');
-//       link.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
+usersSchema.method('hashPassword', function(){
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.password, null, null).bind(this);
+});
 
-// db.knex.schema.hasTable('users').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('users', function (user) {
-//       user.increments('id').primary();
-//       user.string('username', 100).unique();
-//       user.string('password', 100);
-//       user.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
+exports.usersSchema = usersSchema;
 
-// module.exports = db;
